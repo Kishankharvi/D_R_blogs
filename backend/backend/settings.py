d2004f5099dd4a7261f1006de4a01c4d3c02abcd
KishanKharvi <kishankharvi702@gmail.com>
2024-10-09 18:54:07 +0530

    added viws

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,13 +134,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL='media/'
 MEDIA_ROOT=BASE_DIR/'media'
-
-REST_FRAMEWORK={
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
-    )
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
+
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=50),
